bed7f238a647c0738d0cf998dcc48cc974fafe40
phanhuyhieu2004 <hieuhuyphantn@gmail.com>
2024-01-18 08:31:48 +0700

    b4th

---
+++ src/main/java/com/example/customermanagementstoredprocedure/service/ICustomerService.java
@@ -0,0 +1,8 @@
+package com.example.customermanagementstoredprocedure.service;
+
+
+import com.example.customermanagementstoredprocedure.model.Customer;
+
+public interface ICustomerService {
+    boolean saveWithStoredProcedure(Customer customer);
+}
