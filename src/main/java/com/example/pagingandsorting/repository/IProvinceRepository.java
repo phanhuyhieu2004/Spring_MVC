b77e064fb015e985d0e7b50da08a431f7e4bdf54
phanhuyhieu2004 <hieuhuyphantn@gmail.com>
2024-01-18 08:21:54 +0700

    b7th

---
+++ src/main/java/com/example/pagingandsorting/repository/IProvinceRepository.java
@@ -0,0 +1,8 @@
+package com.example.pagingandsorting.repository;
+
+
+import com.example.pagingandsorting.model.Province;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IProvinceRepository extends CrudRepository<Province, Long> {
+}
