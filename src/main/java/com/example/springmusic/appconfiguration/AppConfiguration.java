91bfd58a648990fa4fddb9bd16df77204ac168eb
Phan Huy Hiệu <114344641+phanhuyhieu2004@users.noreply.github.com>
2024-03-08 17:00:37 +0700

    Update AppConfiguration.java

--- src/main/java/com/example/springmusic/appconfiguration/AppConfiguration.java
+++ src/main/java/com/example/springmusic/appconfiguration/AppConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.springmusic.appconfiguration;
+package com.example.springmusic.appconfigurationnnnn;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
